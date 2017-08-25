44ce8603a6632ba35e315900e39d25b69600cee7
Eugene Khashin <adastreamer@gmail.com>
2017-08-25 11:23:05 +0300

    Procfile added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('build'))
 
-app.listen(3000, function() {
-    console.log('app listening on port 3000! http://localhost:3000');
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 // test ss
