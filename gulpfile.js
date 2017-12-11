cbab143829626bfb4ed11a179b3de82be9ac6f2d
Горлачев Артем <agorlachev@rbc.ru>
2017-12-11 14:42:40 +0300

    btc со ссылкой на bitcointalk

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ var structure = {
             ],
             socials: [
                 '<a href="https://t.me/bananacoineng" onClick="sendEvent(\'ViewContent\')" target="_blank" class="fa fa-telegram"></a>',
+                '<a href="https://bitcointalk.org/index.php?topic=2112748" onClick="sendEvent(\'ViewContent\')" target="_blank" class="fa fa-btc"></a>',
                 '<a href="/img/qrcode_for_bananacoin_wechat.jpg" target="_blank" class="fa fa-wechat"></a>',
                 '<a href="https://twitter.com/Bananacoin_Eng" target="_blank" class="fa fa-twitter"></a>',
                 '<a href="https://www.facebook.com/bananacoinENG/" target="_blank" class="fa fa-facebook-f"></a>',
